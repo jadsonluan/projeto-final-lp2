8334745640ea4f13ddb68066740654b3a8d40939
Gabriel de Sousa <gabriel.sb199@gmail.com>
2018-03-04 19:48:46 -0300

    Classe Caixa
    Classe responsável por gerenciar o dinheiro do sistema

---
+++ src/principal/Caixa.java
@@ -0,0 +1,35 @@
+package principal;
+
+public class Caixa {
+	private int dinheiro;
+	
+	public Caixa() {
+		this.dinheiro = 0;
+	}
+	
+	public int getDinheiro() {
+		return dinheiro;
+	}
+	
+	/**
+	 * Adiciona dinheiro ao caixa
+	 * @param dinheiro 
+	 * 			dinheiro a ser adicionado ao caixa 			
+	 */
+	public void adicionaDinheiro(int dinheiro) {
+		this.dinheiro += dinheiro;
+	}
+	
+	/**
+	 * Retira dinheiro do caixa
+	 * @param dinheiro
+	 * 			dinheiro a ser retirado do caixa
+	 */
+	public void removeDinheiro(int dinheiro) {
+		if(this.dinheiro > dinheiro) {
+			this.dinheiro -= dinheiro;
+		}else {
+			throw new IllegalArgumentException("Erro ao retirar dinheiro do caixa: dinheiro insuficiente");
+		}
+	}
+}
