5e6fcac47db80b4b7ebcb293ff8ff2802d339d3b
Luan <jadsonluan1@gmail.com>
2018-03-04 15:16:31 -0300

    Corrige espaçamento

--- src/principal/Ajuda.java
+++ src/principal/Ajuda.java
@@ -1,12 +1,10 @@
 package principal;
 
-
 public abstract class Ajuda {
 	private int id;
 	private String matrAluno;
 	private String matrTutor;
 	private String disciplina;
-	
 	
 	public Ajuda(int id, String matrAluno, String matrTutor, String disciplina) {
 		this.id = id;
@@ -15,20 +13,19 @@ public abstract class Ajuda {
 		this.disciplina = disciplina;
 	}
 
-
-
 	public String getInfo(String atributo){
 		String valor = null;
+		
 		if(atributo.equals("matrAluno")){
 			valor = this.matrAluno;
 		}else if(atributo.equals("disciplina")){
 			valor = this.disciplina;
 		}
+		
 		return valor;
 	}
 	
 	public String getTutor(){
 		return this.matrTutor;
 	}
-	
 }
