26f8c9b6e438e0d2b6b3e545bce09cb0bb9aab62
Luan <jadsonluan1@gmail.com>
2018-03-04 15:12:01 -0300

    Corrige espaçamento e casting

--- src/principal/AjudaController.java
+++ src/principal/AjudaController.java
@@ -7,25 +7,29 @@ public class AjudaController {
 	private Map<Integer, Ajuda> listaAjuda;
 
 	public AjudaController() {
-		this.listaAjuda = new HashMap<Integer, Ajuda>();
+		this.listaAjuda = new HashMap<>();
 	}
-	
-    public int pedirAjudaPresencial (String matrAluno, String matrTutor, String disciplina, String horario, String dia, String localInteresse){
-    	int id = this.listaAjuda.values().size()+1;
-    	this.listaAjuda.put(id, new AjudaPresencial(id,matrAluno, matrTutor, disciplina, horario, dia, localInteresse));
-    	return id;
-    }
-    public int pedirAjudaOnline (String matrAluno, String matrTutor, String disciplina){
-    	int id = this.listaAjuda.values().size()+1;
-    	this.listaAjuda.put(id, new AjudaOnline(id,matrAluno, matrTutor, disciplina));
-    	return id;
-    }
-    public String pegarTutor(int idAjuda){
-    	return this.listaAjuda.get((Integer) idAjuda).getTutor();
-    }
-    public String getInfoAjuda(int idAjuda, String atributo){
-    	return this.listaAjuda.get((Integer) idAjuda).getInfo(atributo);
-    }
 
+	public int pedirAjudaPresencial(String matrAluno, String matrTutor, String disciplina, String horario, String dia,
+			String localInteresse) {
+		int id = this.listaAjuda.values().size() + 1;
+		this.listaAjuda.put(id,
+				new AjudaPresencial(id, matrAluno, matrTutor, disciplina, horario, dia, localInteresse));
+		return id;
+	}
+
+	public int pedirAjudaOnline(String matrAluno, String matrTutor, String disciplina) {
+		int id = this.listaAjuda.values().size() + 1;
+		this.listaAjuda.put(id, new AjudaOnline(id, matrAluno, matrTutor, disciplina));
+		return id;
+	}
+
+	public String pegarTutor(int idAjuda) {
+		return this.listaAjuda.get(idAjuda).getTutor();
+	}
+
+	public String getInfoAjuda(int idAjuda, String atributo) {
+		return this.listaAjuda.get(idAjuda).getInfo(atributo);
+	}
 
 }
