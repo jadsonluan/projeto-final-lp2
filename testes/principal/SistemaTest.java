8f15bf87ff24f2eef3eeb6e95089529c1b0f1bb9
Jadson Luan <jadsonluan1@gmail.com>
2018-03-06 08:56:04 -0300

    Atualiza us5 e us6 do easy accept

--- testes/principal/SistemaTest.java
+++ testes/principal/SistemaTest.java
@@ -20,7 +20,10 @@ public class SistemaTest {
 	public void criaSistema() {
 		sistema = new Sistema();
 		sistema.cadastrarAluno("Matheus", "1001", 9999, "4000-0004", "matheus.g@ccc.ufcg.edu.br");
+		sistema.cadastrarAluno("Gabriel", "1111", 10000, "0000-1000", "gabriel@ccc.ufcg.edu.br");
 		sistema.tornarTutor("1001", "Programação 2", 5);
+		sistema.cadastrarHorario("matheus.g@ccc.ufcg.edu.br", "15:00", "sex");
+		sistema.cadastrarLocalDeAtendimento("matheus.g@ccc.ufcg.edu.br", "LCC2");
 	}
 	
 	@Test
@@ -183,5 +186,104 @@ public class SistemaTest {
 	@Test(expected = IllegalArgumentException.class)
 	public void testPegarNivelTutorInexistente() {
 		sistema.pegarNivel("101");
+	}
+	
+	@Test
+	public void testPedirAjudaPresencial() {
+		int id = sistema.pedirAjudaPresencial("1111", "Programação 2", "15:00", "sex", "LCC2");
+		assertEquals(1, id);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testPedirAjudaPresencialMatrAlunoVazia() {
+		sistema.pedirAjudaPresencial("     ", "Programação 2", "15:00", "sex", "LCC2");
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testPedirAjudaPresencialMatrAlunoNula() {
+		sistema.pedirAjudaPresencial(null, "Programação 2", "15:00", "sex", "LCC2");
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testPedirAjudaPresencialDisciplinaVazia() {
+		sistema.pedirAjudaPresencial("1111", "     ", "15:00", "sex", "LCC2");
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testPedirAjudaPresencialDisciplinaNula() {
+		sistema.pedirAjudaPresencial("1111", null, "15:00", "sex", "LCC2");
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testPedirAjudaPresencialHorarioVazio() {
+		sistema.pedirAjudaPresencial("1111", "Programação 2", "     ", "sex", "LCC2");
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testPedirAjudaPresencialHorarioNulo() {
+		sistema.pedirAjudaPresencial("1111", "Programação 2", null, "sex", "LCC2");
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testPedirAjudaPresencialDiaVazio() {
+		sistema.pedirAjudaPresencial("1111", "Programação 2", "15:00", "    ", "LCC2");
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testPedirAjudaPresencialDiaNulo() {
+		sistema.pedirAjudaPresencial("1111", "Programação 2", "15:00", null, "LCC2");
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testPedirAjudaPresencialLocalVazio() {
+		sistema.pedirAjudaPresencial("1111", "Programação 2", "15:00", "sex", "     ");
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testPedirAjudaPresencialLocalNulo() {
+		sistema.pedirAjudaPresencial("1111", "Programação 2", "15:00", "sex", null);
+	}
+	
+	@Test
+	public void testPedirAjudaOnline() {
+		int id = sistema.pedirAjudaOnline("1111", "Programação 2");
+		assertEquals(1, id);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testPedirAjudaOnlineMatrAlunoVazia() {
+		sistema.pedirAjudaOnline("     ", "Programação 2");
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testPedirAjudaOnlineMatrAlunoNula() {
+		sistema.pedirAjudaOnline(null, "Programação 2");
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testPedirAjudaOnlineDisciplinaVazia() {
+		sistema.pedirAjudaOnline("1111", "     ");
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testPedirAjudaOnlineDisciplinaNula() {
+		sistema.pedirAjudaOnline("1111", null);
+	}
+	
+	@Test
+	public void testPegarTutorAjudaOnline() {
+		int id = sistema.pedirAjudaOnline("1111", "Programação 2");
+		assertEquals("Tutor - 1001, disciplina - Programação 2", sistema.pegarTutor(id));
+	}
+	
+	@Test
+	public void testPegarTutorAjudaPresencial() {
+		int id = sistema.pedirAjudaPresencial("1111", "Programação 2", "15:00", "sex", "LCC2");
+		assertEquals("Tutor - 1001, horario - 15:00, dia - sex, local - LCC2, disciplina - Programação 2", sistema.pegarTutor(id));
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testPegarTutorAjudaInexistente() {
+		sistema.pegarTutor(1);
 	}
 }
